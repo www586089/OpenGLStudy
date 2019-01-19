fcb964c71c9df721ee5e2591c99c2cdcb5bdfc3d
Little <414929680@qq.com>
2019-01-19 15:58:53 +0800

    AirHockey2

---
+++ gles2for-android/AirHockey2/app/src/main/java/com/airhockey/android/AirHockeyRenderer.java
@@ -0,0 +1,124 @@
+package com.airhockey.android;
+
+import android.content.Context;
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+
+import com.airhockey.android.util.LoggerConfig;
+import com.airhockey.android.util.ShaderHelper;
+import com.airhockey.android.util.TextResourceReader;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import static android.opengl.GLES20.GL_FLOAT;
+import static android.opengl.GLES20.GL_LINES;
+import static android.opengl.GLES20.GL_POINTS;
+import static android.opengl.GLES20.GL_TRIANGLES;
+import static android.opengl.GLES20.GL_TRIANGLE_FAN;
+import static android.opengl.GLES20.glDrawArrays;
+import static android.opengl.GLES20.glEnableVertexAttribArray;
+import static android.opengl.GLES20.glGetAttribLocation;
+import static android.opengl.GLES20.glGetUniformLocation;
+import static android.opengl.GLES20.glUniform4f;
+import static android.opengl.GLES20.glUseProgram;
+import static android.opengl.GLES20.glVertexAttribPointer;
+
+public class AirHockeyRenderer implements GLSurfaceView.Renderer {
+
+    private Context context;
+    private static final int POSITION_COMPONENT_COUNT = 2;
+    private static final int BYTES_PER_FLOAT = 4;
+    private FloatBuffer vertexData;
+    private int program;
+
+
+    private float[] tableVertices = {
+            0f, 0f,
+            0f, 14f,
+            9f, 14f,
+            9f, 0f
+    };
+    private float[] tableVerticesWithTriangles = {
+            // Order of coordinates: X, Y, R, G, B
+            // Triangle Fan
+               0f,    0f,   1f,   1f,   1f,
+            -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+             0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+             0.5f,  0.5f, 0.7f, 0.7f, 0.7f,
+            -0.5f,  0.5f, 0.7f, 0.7f, 0.7f,
+            -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+            // Line 1
+            -0.5f, 0f, 1f, 0f, 0f,
+            0.5f, 0f, 1f, 0f, 0f,
+            // Mallets
+            0f, -0.25f, 0f, 0f, 1f,
+            0f, 0.25f, 1f, 0f, 0f
+    };
+    private static final String U_COLOR = "u_Color";
+    private int uColorLocation;
+    private static final String A_POSITION = "a_Position";
+    private int aPositionLocation;
+
+    private static final String A_COLOR = "a_Color";
+    private static final int COLOR_COMPONENT_COUNT = 3;
+    private static final int STRIDE = (POSITION_COMPONENT_COUNT + COLOR_COMPONENT_COUNT) * BYTES_PER_FLOAT;
+    private int aColorLocation;
+    public AirHockeyRenderer(Context context) {
+        this.context = context;
+        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(tableVerticesWithTriangles.length * BYTES_PER_FLOAT);
+        byteBuffer.order(ByteOrder.nativeOrder());
+        byteBuffer.position(0);
+        vertexData = byteBuffer.asFloatBuffer();
+        vertexData.put(tableVerticesWithTriangles);
+    }
+
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        String vertexShaderSource = TextResourceReader.readTextFileFromResource(context, R.raw.simple_vertex_shader);
+        String fragmentShaderSource = TextResourceReader.readTextFileFromResource(context, R.raw.simple_fragment_shader);
+
+        int vertexShader = ShaderHelper.compileVertexShader(vertexShaderSource);
+        int fragmentShader = ShaderHelper.compileFragmentShader(fragmentShaderSource);
+        program = ShaderHelper.linkProgram(vertexShader, fragmentShader);
+        if (LoggerConfig.ON) {
+            ShaderHelper.validateProgram(program);
+        }
+        glUseProgram(program);
+        aColorLocation = glGetAttribLocation(program, A_COLOR);
+        aPositionLocation = glGetAttribLocation(program, A_POSITION);
+
+        vertexData.position(0);
+        glVertexAttribPointer(aPositionLocation, POSITION_COMPONENT_COUNT, GL_FLOAT, false, STRIDE, vertexData);
+        glEnableVertexAttribArray(aPositionLocation);
+
+        vertexData.position(POSITION_COMPONENT_COUNT);
+        glVertexAttribPointer(aColorLocation, COLOR_COMPONENT_COUNT, GL_FLOAT, false, STRIDE, vertexData);
+        glEnableVertexAttribArray(aColorLocation);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        GLES20.glViewport(0, 0, width, height);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+
+        glDrawArrays(GL_TRIANGLE_FAN, 0, 6);
+        glDrawArrays(GL_LINES, 6, 2);
+
+        // Draw the first mallet blue.
+        glDrawArrays(GL_POINTS, 8, 1);
+        // Draw the second mallet red.
+        glDrawArrays(GL_POINTS, 9, 1);
+
+    }
+}
